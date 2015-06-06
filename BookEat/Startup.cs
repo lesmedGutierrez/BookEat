6f5aa50fb0efc475bb168d0b15061965aa73218a
Lesmed Gutiérrez Arcia <lesmedgutierrez@outlook.com>
2015-06-06 17:44:54 -0600

    first commit

---
+++ BookEat/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BookEat.Startup))]
+namespace BookEat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
