0392d6b01fa854c5ec89cfb2a52e700bf3e2561d
Jonathan Solís Parajeles <jonathan.solis.parajeles@gmail.com>
2015-06-10 11:16:14 -0600

    Required info added

--- BookEat/Models/Restaurant.cs
+++ BookEat/Models/Restaurant.cs
@@ -11,15 +11,23 @@ namespace BookEat.Models
     public class Restaurant
     {
         public int RestaurantID { get; set; }
-        [StringLength(150)]
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public string Cuisine { get; set; }
+        [Required]
         public string State { get; set; }
+        [Required]
         public string Address { get; set; }
+        [Required]
         public string PhoneNumber { get; set; }
+        [EmailAddress]
+        [Required]
         public string Email { get; set; }
         public int Rating { get; set; }
+        [Required]
         public int Tables { get; set; }
     }
 }
